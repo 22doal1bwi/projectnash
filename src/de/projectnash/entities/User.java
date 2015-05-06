7bfee2ebf83fa6fc48b05073c7a0b78e3e48aa73
22erar1bwi <22erar1bwi@hft-stuttgart.de>
2015-05-06 20:36:18 +0200

    Unnötiger Import entfernt

--- src/de/projectnash/entities/User.java
+++ src/de/projectnash/entities/User.java
@@ -15,8 +15,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.sun.istack.internal.NotNull;
-
 /**
  * This class provides a realistic {@link User} with all its attributes.
  * 
