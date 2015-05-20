7084947bbe8cd9f1a5d565fa6b60c0d0e058b717
Marius Boepple <22boma1bwi@hft-stuttgart.de>
2015-05-20 20:57:42 +0200

    Added revokeCertificateServlet

---
+++ src/de/projectnash/frontend/servlets/RevokeCertificateServlet.java
@@ -0,0 +1,68 @@
+package de.projectnash.frontend.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import de.projectnash.application.CertificateLogic;
+import de.projectnash.application.UserLogic;
+import de.projectnash.entities.User;
+import de.projectnash.frontend.controllers.SessionController;
+
+/**
+ * Servlet implementation class RevokeCertificateServlet
+ */
+@WebServlet("/RevokeCertificateServlet")
+public class RevokeCertificateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		String sessionIdStatus = SessionController.checkForSessionId(request,
+				response);
+		
+		User user = UserLogic.loadUserBySession(sessionIdStatus);
+		
+		switch (sessionIdStatus) {
+		default:
+			boolean revokedCertificate = CertificateLogic.revokeCertificate(user);
+			
+				if (revokedCertificate) {
+					map.put("revokedCertificate", true);
+
+				} else {
+					map.put("revokedCertificate", false);
+				}
+					map.put("validSession", true);
+		break;
+
+		case "0":
+			map.put("validSession", false);
+			break;
+
+		case "-1":
+			map.put("validSession", false);
+			break;
+		}
+		write(response, map);
+	}
+
+	private void write(HttpServletResponse resp, Map<String, Object> map)
+			throws IOException {
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().write(new Gson().toJson(map));
+	}
+}
