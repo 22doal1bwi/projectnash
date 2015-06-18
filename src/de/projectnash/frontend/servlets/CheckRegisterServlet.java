d5761ed7114486c8219440e846322e02662a65d0
Silvio D'Alessandro <22dasi1bwi@hft-stuttgart.de>
2015-06-18 19:10:09 +0200

    Refactored CheckRegisterServlet.java

--- src/de/projectnash/frontend/servlets/CheckRegisterServlet.java
+++ src/de/projectnash/frontend/servlets/CheckRegisterServlet.java
@@ -10,44 +10,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
 import de.projectnash.application.UserLogic;
+import de.projectnash.application.util.ServletResponseHandler;
 
 /**
  * Servlet implementation class CheckRegisterServlet
  */
 @WebServlet("/CheckRegisterServlet")
 public class CheckRegisterServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	private static final String PERSONAL_ID = "personalId";
+	
+	private static final String E_MAIL_ADDRESS = "emailAddress";
+	
+	private static final String ALREADY_EXISTS = "alreadyExists";
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Map<String, Object> map = new HashMap<String, Object>();
+		String personalId = request.getParameter(PERSONAL_ID);
+		String emailAddress = request.getParameter(E_MAIL_ADDRESS);
 		boolean inputAlreadyExists = false;
-		
-		String personalId = request.getParameter("personalId");
-		String emailAddress = request.getParameter("emailAddress");
 
 		if (personalId != null && !personalId.isEmpty()) {
 			inputAlreadyExists = UserLogic.personalIdAlreadyExists(personalId);
 		} else if (emailAddress != null && !emailAddress.isEmpty()) {
 			inputAlreadyExists = UserLogic.emailAlreadyExists(emailAddress);
 		}
-
-		map.put("alreadyExists", inputAlreadyExists);
-		write(response, map);
-	};
-
-	private void write(HttpServletResponse resp, Map<String, Object> map)
-			throws IOException {
-		resp.setContentType("application/json");
-		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().write(new Gson().toJson(map));
+		map.put(ALREADY_EXISTS, inputAlreadyExists);
+		ServletResponseHandler.write(response, map);
 	}
 }
