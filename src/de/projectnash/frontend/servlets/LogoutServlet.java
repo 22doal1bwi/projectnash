cea5082782c8ad068449eaa543a3395bf5b6f733
Silvio D'Alessandro <22dasi1bwi@hft-stuttgart.de>
2015-06-18 19:28:45 +0200

    Refactored LogoutServlet.java

--- src/de/projectnash/frontend/servlets/LogoutServlet.java
+++ src/de/projectnash/frontend/servlets/LogoutServlet.java
@@ -17,12 +17,13 @@ public class LogoutServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		
-		// invalidate the session if exists
+		/* invalidate the session if exists. */
 		HttpSession session = request.getSession(false);
 		
 		if (session != null) {
